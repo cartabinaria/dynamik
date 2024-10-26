5fe9f98774089ce4da3fc7bf8cd6b22d0a5b2ae4
VaiTon <eyadlorenzo@gmail.com>
2024-10-26 17:26:00 +0200

    fix: new page and matcher for .pdf files

---
+++ src/routes/[...dir]/[file=pdf]/+page.ts
@@ -0,0 +1,17 @@
+import type { PageLoad } from './$types';
+import { ASSET_URL } from '$lib/const';
+import { redirect } from '@sveltejs/kit';
+
+export const load: PageLoad = async ({ params }) => {
+	const fileUrl = ASSET_URL(params.dir + '/' + params.file);
+
+	// check if the user agent is iOS
+	const isIOS = !import.meta.env.SSR && navigator.userAgent.match(/(iPad|iPhone|iPod)/g); // ! workaround
+	if (isIOS) {
+		// redirect to the original file
+		redirect(302, fileUrl);
+	}
+	return {
+		url: fileUrl
+	};
+};
