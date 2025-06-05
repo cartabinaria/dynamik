63a5f44c74cba1f8f9e350d98e9c3ccfe2ba6b31
Stefano Volpe <stefano.volpe@student.uva.nl>
2025-06-06 01:32:20 +0200

    fix: status page logic (#414)

--- src/routes/build/+page.svelte
+++ src/routes/build/+page.svelte
@@ -23,34 +23,37 @@
 	</nav>
 	<div class="m-8">
 		{#each data.degrees as degree, i}
-			{#if i > 0}
-				<hr class="my-8 border-primary" />
-			{/if}
-			<h2 class="text-center text-2xl">{degree.name}</h2>
 			{#if degree.teachings != null}
-				{#each [...Array(MAX_YEARS_FOR_DEGREE).keys()].map((i) => i + 1) as year}
-					<h3 class="text-center text-xl font-bold my-4">{year}</h3>
-					<div class="grid grid-cols-4 gap-4">
-						{#each teachingsFilter(degree, year) as teaching}
-							<div>
-								<h4 class="font-bold">{teaching}</h4>
-								{#if data.activeTeachings[i].includes(teaching)}
-									<div class="flex gap-2">
-										{#each WORKFLOW_NAMES as workflow}
-											{@const url = data.teachings.get(teaching)?.url}
-											{#if url != null}
-												{@const href = WORKFLOW_URL(url, workflow)}
-												{@const src = `${href}/badge.svg`}
-												<a {href}>
-													<img {src} alt="Not found" />
-												</a>
-											{/if}
-										{/each}
-									</div>
-								{/if}
-							</div>
-						{/each}
-					</div>
+				{#if i > 0}
+					<hr class="my-8 border-primary" />
+				{/if}
+				<h2 class="text-center text-2xl">{degree.name}</h2>
+				{#each [...Array(MAX_YEARS_FOR_DEGREE).keys()].map((i) => i + 1).concat([0]) as year}
+					{@const teachings = teachingsFilter(degree, year)}
+					{#if teachings.length > 0}
+						<h3 class="text-center text-xl font-bold my-4">{year || 'Senza anno'}</h3>
+						<div class="grid grid-cols-4 gap-4">
+							{#each teachings as teaching}
+								<div>
+									<h4 class="font-bold">{teaching}</h4>
+									{#if data.activeTeachings[i].includes(teaching)}
+										<div class="flex gap-2">
+											{#each WORKFLOW_NAMES as workflow}
+												{@const url = data.teachings.get(teaching)?.url}
+												{#if url != null}
+													{@const href = WORKFLOW_URL(url, workflow)}
+													{@const src = `${href}/badge.svg`}
+													<a {href}>
+														<img {src} alt="Not found" />
+													</a>
+												{/if}
+											{/each}
+										</div>
+									{/if}
+								</div>
+							{/each}
+						</div>
+					{/if}
 				{/each}
 			{/if}
 		{/each}
