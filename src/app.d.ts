19c171c24c19051ca991d6963eec3d9a936cd0d1
VaiTon <eyadlorenzo@gmail.com>
2025-06-28 18:38:39 +0200

    Move octokit to devDependencies and clean up app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,17 +3,6 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// GitHub user type for session
-		interface GitHubUser {
-			login: string;
-			id: number;
-			avatar_url: string;
-			html_url: string;
-			name?: string;
-			email?: string;
-			// Add more fields as needed from GitHub API
-		}
-
 		interface Locals {
 			github_access_token?: string;
 		}
