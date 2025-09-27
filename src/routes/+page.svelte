f7d83a225bdd9f08e14b3df28f4d74643678cea0
Alice Benatti <alice17bee@gmail.com>
2025-09-27 14:52:44 +0200

    style: new home page by categories

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,18 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	import { setBannerClosed, shouldShowBanner } from '$lib/newsBanners';
 
 	let { data }: { data: { degrees: Degree[] } } = $props();
+
+	let degreeAggregates: Record<string, string[]> = {
+		informatica: ['informatica', 'informatica-per-il-management', 'informatica-magistrale'],
+		ingegneria: ['ingegneria', 'ingegneria-informatica-magistrale', 'artificial-intelligence'],
+		cesena: ['ingegneria-e-scienze-informatiche-magistrale'],
+		fisica: ['fisica'],
+		altro: ['lab']
+	};
+
+	function checkDegreeCategory(data: { degrees: Degree[] }, category: string): Degree[] {
+		return data.degrees.filter((degree) => degreeAggregates[category]?.includes(degree.id));
+	}
 </script>
 
 <svelte:head>
@@ -55,12 +67,15 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 	</div>
 {/if}
 
-{#snippet line(name: string, href: string, icon: string)}
-	<li>
-		<a class="py-8 justify-center text-center border-base-content border-2 mb-4" {href}>
-			{icon}
-			{name}
-			{icon}
+{#snippet line(name: string, href: string, icon: string, tools?: boolean)}
+	<li class="flex-auto">
+		<a
+			class="flex justify-center items-center p-4 rounded-lg hover:shadow-lg hover:bg-accent/80 transition w-full
+         transition-all duration-300 ease-in-out {tools ? 'bg-base-300' : 'border'}"
+			{href}
+		>
+			<span class="text-3xl mb-2">{icon}</span>
+			<span class="font-semibold text-center">{name}</span>
 		</a>
 	</li>
 {/snippet}
@@ -85,24 +100,36 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 			</p>
 		</div>
 
-		<ul class="menu p-2 text-lg mt-8 w-full">
-			{#each data.degrees as degree (degree.id)}
-				{#if degree.teachings != null}
-					{@render line(degree.name, `${base}/dash/${degree.id}`, degree.icon)}
-				{:else}
-					{@render line(degree.name, `${base}/${degree.id}`, degree.icon)}
-				{/if}
-			{/each}
+		<ul class="menu p-2 text-lg w-full mt-2">
+			<div class="grid gap-6 sm:grid-cols-2">
+				{#each Object.keys(degreeAggregates) as category}
+					{#if checkDegreeCategory(data, category).length > 0}
+						<div class="p-4 hover:shadow-lg transition bg-base-300 rounded-2xl">
+							<!-- Corsi della categoria -->
+							<div class="grid gap-2">
+								{#each checkDegreeCategory(data, category) as degree (degree.id)}
+									{#if degree.teachings != null}
+										{@render line(degree.name, `${base}/dash/${degree.id}`, degree.icon)}
+									{:else}
+										{@render line(degree.name, `${base}/${degree.id}`, degree.icon)}
+									{/if}
+								{/each}
+							</div>
+						</div>
+					{/if}
+				{/each}
+			</div>
 
 			<div class="divider"></div>
 
-			<div class="grid grid-cols-2 gap-8">
-				{@render line('Impostazioni', `${base}/settings`, '🔧')}
-				{@render line('Stato', `${base}/build`, '📊')}
+			<div class="flex justify-between flex-wrap gap-2 sm:gap-4">
+				{@render line('FAQ', `${base}/faq`, '❓', true)}
+				{@render line('Impostazioni', `${base}/settings`, '🔧', true)}
+				{@render line('Stato', `${base}/build`, '📊', true)}
 			</div>
 		</ul>
 
-		<div class="alert alert-warning md:mx-4 md:my-8 grow mx-2 sm:mx-auto" role="alert">
+		<div class="alert alert-warning md:mx-4 md:my-8 grow mx-2 sm:mx-auto rounded-xl" role="alert">
 			<span class="icon-[ph--hand-palm-fill] text-4xl"></span>
 			<div>
 				<p class="block font-bold">
@@ -124,7 +151,7 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 
 		<div class="my-8">
 			<p class="text-center">
-				<a href="https://t.me/cartabinaria" class="btn btn-ghost">
+				<a href="https://t.me/cartabinaria" class="btn btn-ghost rounded-xl">
 					<span class="icon-[ph--telegram-logo-fill] text-2xl"></span>
 					Contattaci su Telegram
 				</a>
